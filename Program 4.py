0b30c82f02083e7059ff449286006f755ce735e1
Vasudha-01 <120655215+Vasudha-01@users.noreply.github.com>
2023-07-28 15:24:54 +0530

    Update Program 4.py

--- Program 4.py
+++ Program 4.py
@@ -56,7 +56,9 @@ merge_sort(my_list)
 print(my_list)
 
 
-#4 B
+
+#4 B program
+#Roman
 def roman2Dec(romStr):
     roman_dict ={'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
  # Analyze string backwards
